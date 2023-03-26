6d60502a4f2bebeb782f6dea9e75a0217a9d3cf6
Muhammad Fadhil Ardiansyah S <mfadhil661@gmail.com>
2023-03-26 21:55:48 +0700

    implementasi design by contract

--- tpmodul6_1302210006/Program.cs
+++ tpmodul6_1302210006/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using tpmodul6_1302210006;
 
-SayaTubeVideo_1302210006 sy = new SayaTubeVideo_1302210006(
-    "Tutorial Design By Contract – [Muhammad Fadhil Ardiansyah Supiyan_PRAKTIKAN]"
-);
-sy.PrintVideoDetails_1302210006();
+string title = "Tutorial Design By Contract – [Muhammad Fadhil Ardiansyah Supiyan_PRAKTIKAN]";
+
+SayaTubeVideo_1302210006 v1 = new SayaTubeVideo_1302210006(title);
+v1.IncreasePlayCount_1302210006(10000001);
+v1.PrintVideoDetails_1302210006();
--- tpmodul6_1302210006/SayaTubeVideo_1302210006.cs
+++ tpmodul6_1302210006/SayaTubeVideo_1302210006.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,23 +15,32 @@ namespace tpmodul6_1302210006
         private int playCount;
 
         public SayaTubeVideo_1302210006(string title) {
+            Debug.Assert(title.Length <= 100 && title != null, "Judul video memiliki panjang maksimal 100 karakter dan tidak berupa null");
+            //generate random id
             Random rdm = new Random();
-            this.id = rdm.Next(10000,99999);
+            this.id = rdm.Next(10000, 99999);
             this.title = title;
             this.playCount = 0;
         }
 
         public void IncreasePlayCount_1302210006(int playCount) {
-            this.playCount += playCount;            
+            Debug.Assert(title.Length <= 100 && title != null, "Judul video memiliki panjang maksimal 100 karakter dan tidak berupa null");
+            try
+            {
+                this.playCount = checked(this.playCount + playCount);
+            }
+            catch (OverflowException e) {
+                Console.WriteLine(e.Message);
+            }
+                        
         }
 
         public void PrintVideoDetails_1302210006() {
-            Console.WriteLine(
-                "Video Details: \n" + 
-                "ID: " + this.id + "\n" + 
-                "Title: " + this.title + "\n" +
-                "Play Count: " + this.playCount
-            );
+            Console.WriteLine("Video Details: ");
+            Console.WriteLine("ID: " + this.id);
+            Console.WriteLine("Title: " + this.title);
+            Console.WriteLine("Play Count: " + this.playCount);
+            Console.WriteLine(" ");
         }
 
     }
