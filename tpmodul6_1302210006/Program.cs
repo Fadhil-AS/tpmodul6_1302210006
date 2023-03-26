3630602a3a28241401fa2b1e11b811a5eaff3136
Muhammad Fadhil Ardiansyah S <mfadhil661@gmail.com>
2023-03-26 20:29:12 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302210006/Program.cs
+++ tpmodul6_1302210006/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmodul6_1302210006;
+
+SayaTubeVideo_1302210006 sy = new SayaTubeVideo_1302210006(
+    "Tutorial Design By Contract – [Muhammad Fadhil Ardiansyah Supiyan_PRAKTIKAN]"
+);
+sy.PrintVideoDetails_1302210006();
---
+++ tpmodul6_1302210006/SayaTubeVideo_1302210006.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302210006
+{
+    internal class SayaTubeVideo_1302210006
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo_1302210006(string title) {
+            Random rdm = new Random();
+            this.id = rdm.Next(10000,99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount_1302210006(int playCount) {
+            this.playCount += playCount;            
+        }
+
+        public void PrintVideoDetails_1302210006() {
+            Console.WriteLine(
+                "Video Details: \n" + 
+                "ID: " + this.id + "\n" + 
+                "Title: " + this.title + "\n" +
+                "Play Count: " + this.playCount
+            );
+        }
+
+    }
+}
